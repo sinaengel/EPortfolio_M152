a4ae883c6d6ee006ec59b54658723d7642c9b2b8
sinaengel <sina.engel@ksb-sg.ch>
2021-12-17 00:49:52 +0100

    navbar

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const main = document.getElementById("mainpage");
+const vorbereitung = document.getElementById("trainingsvorbereitung");
+const durchfuehrung = document.getElementById("durchfuehrung");
+const elemente = document.getElementById("elemente");
+const video = document.getElementById("video");
+
+const btnhome = document.getElementById("navHome");
+const btnvorbereitung = document.getElementById("#navTrainingsvorbereitung");
+const btndurchführung = document.getElementById("navDurchfuehrung");
+const btnausruestung = document.getElementById("navAusruestung");
+const btnVideo = document.getElementById("navVideo");
+
+
+
+
+
